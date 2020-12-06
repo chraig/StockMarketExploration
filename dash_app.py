ee0921603ce21577042a7a3804e911266e51ce24
Christoph Aigner <3903786+chraig@users.noreply.github.com>
2020-12-06 12:57:43 +0100

    Added simple ticker selection

--- dash_app.py
+++ dash_app.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 import charts
 import definitions
@@ -21,18 +21,35 @@ app = dash.Dash(__name__,
 # Import and clean data (importing csv into pandas)
 df = pd.read_csv("assets/msft_prices.csv")
 # df.set_index("date", inplace=True)
-df['date'] = pd.to_datetime(df['date'], format="%Y-%m-%d %H:%M:%S")
+df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d %H:%M:%S")
+df.rename(columns={"1. open": "open",
+                   "2. high": "high",
+                   "3. low": "low",
+                   "4. close": "close",
+                   "5. volume": "volume"},
+          inplace=True)
 print(df.dtypes)
 print(df[:5])
 
 ticker_selected = "MSFT"
+tickers = ["MSFT"]
 
 
 # ------------------------------------------------------------------------------
 # Sub-divs loaded into app layout
 def charts_div(ticker):
     return html.Div(
-
+        id=ticker+"_graph_div",
+        className="display-none",
+        children=[
+            html.Div(
+                dcc.Graph(
+                    id=ticker+"_chart",
+                    className="chart-graph",
+                    config={"displayModeBar": False, "scrollZoom": True},
+                )
+            ),
+        ]
     )
 
 
@@ -49,11 +66,40 @@ app.layout = html.Div(
                      options=definitions.TIME_SELECTION_OPTIONS,
                      multi=False,
                      clearable=False,
-                     value=definitions.TIME_SELECTION_OPTIONS[0]['value']),
+                     value=definitions.TIME_SELECTION_OPTIONS[2]['value']),
+        dcc.Dropdown(id="ticker_selection",
+                     options=[{"label": "MSFT", "value": "MSFT"}],
+                     multi=False,
+                     clearable=False,
+                     value="MSFT"),
 
+        html.Div(id="output_container", children=[]),
 
-        dcc.Graph(id="charts", className="col-charts",
-                  children=[]),  # children=[charts_div(ticker_selected)]),
+        # dcc.Graph(id="charts", className="col-charts",
+        #           children=[]),  # children=[charts_div(ticker_selected)]),
+
+        # Charts Div
+        html.Div(
+            id="charts",
+            className="charts",
+            children=[
+                # charts_div(ticker_selected),
+                html.Div(
+                    id="msft" + "_graph_div",
+                    className="display-none",
+                    children=[
+                        html.Div(
+                            dcc.Graph(
+                                id="msft" + "_chart",
+                                className="chart-graph",
+                                config={"displayModeBar": False, "scrollZoom": True},
+                                children=[]
+                            ),
+                        ),
+                    ]
+                )
+            ],
+        ),
 
         """
         # Ticker line
@@ -80,11 +126,15 @@ app.layout = html.Div(
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
 @app.callback(
-    Output(component_id='charts', component_property='figure'),
-    Input(component_id='time_selection', component_property='value')
+    Output(component_id='msft_chart', component_property='figure'),
+    [Input(component_id='time_selection', component_property='value'),
+     Input(component_id='ticker_selection', component_property='value')]
 )
-def update_graph(time_selection):
-    print(time_selection)
+def update_graph(time_selection, ticker_selection):
+
+    if ticker_selection not in tickers:
+        fig = go.Figure()
+        return fig
 
     now = datetime.now()
     start_dt = now - timedelta(hours=time_selection)
@@ -92,13 +142,12 @@ def update_graph(time_selection):
 
     dff = df.copy()
     dff = dff[(dff.date >= start_dt) & (dff.date <= end_dt)]
-    print(dff)
 
     fig = go.Figure(charts.candlestick_chart(dff))
 
     fig.update_layout(
         title_text="Stock Price",
-        xaxis_rangeslider_visible=False
+        # xaxis_rangeslider_visible='slider' in value
     )
 
     return fig
